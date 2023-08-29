e0afdf0654f50004456279ffb24f326828740b0f
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-29 02:54:03 -0400

    added logic to Carousel.js that will create a slide show with provided image files

---
+++ my-app/src/components/carousel/Carousel.js
@@ -0,0 +1,30 @@
+// Import React and Carousel components
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+// Import components to use in carousel
+import ScrollDown from "../scroll-down/Scroll-down"
+// Import styles to use in carousel
+import "./carousel.css"
+// Import images to use in carousel
+import slide1 from "../../assets/images/carousel/slide1.jpg"
+import slide2 from "../../assets/images/carousel/slide2.jpg"
+import slide3 from "../../assets/images/carousel/slide3.jpg"
+import slide4 from "../../assets/images/carousel/slide4.jpg"
+
+const CustomCarousel = () => {
+    return (
+        <div className='home'>
+            <Carousel controls ={false}>
+                {/* Map over an array of images in slide show */}
+                {[slide1, slide2, slide3, slide4].map((slide, index) => {
+                    <Carousel.Item key={index}>
+                        <img className="d-block w-100 custome-img" src={slide} alt={`Slide $ {index + 1}`}/>
+                        </Carousel.Item>
+                })}
+                </Carousel>
+                <ScrollDown/>
+                </div>
+    );
+};
+// Export CustomCarousel component
+export default CustomCarousel;
