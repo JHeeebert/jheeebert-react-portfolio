f585332e7d9c24e41dfc7d76af064580f282b55c
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-28 22:22:18 -0400

    updated package.json

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,8 +5,8 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div>
-      <header></header>
-      <footer></footer>
+    {Header()}
+    {Footer()}
     </div>
   );
 }
