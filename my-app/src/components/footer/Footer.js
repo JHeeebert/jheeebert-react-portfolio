ee1752f3d3a41440a64d087f89161d73bc7b853b
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-29 03:42:53 -0400

    added logic to Footer.js

---
+++ my-app/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css";
+// Footer Component
+const Footer = () => {
+    return (
+        <div>
+            <div className="py-2 text-center footer-style">© Joshua Hebert; {new Date().getFullYear()}</div>
+        </div>
+    );
+}
+// Export Footer component
+export default Footer;
