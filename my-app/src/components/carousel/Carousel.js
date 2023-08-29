2c2af8569f5f00e4f54807fb587fb7eed9dc80a5
Joshua Hebert <jhebertcocktails@gmail.com>
2023-08-29 03:14:22 -0400

    added slide images for carousel

--- my-app/src/components/carousel/Carousel.js
+++ my-app/src/components/carousel/Carousel.js
@@ -7,7 +7,7 @@ import ScrollDown from "../scroll-down/Scroll-down"
 import "./carousel.css"
 // Import images to use in carousel
 import slide1 from "../../assets/images/carousel/slide1.jpg"
-import slide2 from "../../assets/images/carousel/slide2.jpg"
+import slide5 from "../../assets/images/carousel/slide5.jpg"
 import slide3 from "../../assets/images/carousel/slide3.jpg"
 import slide4 from "../../assets/images/carousel/slide4.jpg"
 
@@ -16,7 +16,7 @@ const CustomCarousel = () => {
         <div className='home'>
             <Carousel controls ={false}>
                 {/* Map over an array of images in slide show */}
-                {[slide1, slide2, slide3, slide4].map((slide, index) => {
+                {[slide1, slide5, slide3, slide4].map((slide, index) => {
                     <Carousel.Item key={index}>
                         <img className="d-block w-100 custome-img" src={slide} alt={`Slide $ {index + 1}`}/>
                         </Carousel.Item>
